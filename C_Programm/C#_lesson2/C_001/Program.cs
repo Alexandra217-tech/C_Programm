fdb71632a35937dfea13e7c942677b5826131ca6
Alexandra <a.a.lisenkova127@gmail.com>
2022-12-26 01:05:54 +0300

    Сделала третью задачу ко второму семинару

---
+++ C_Programm/C#_lesson2/C_001/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int del1 = number%100;
+int del2 = del1/10;
+Console.WriteLine("Вторая цифра числа: " + del2);
